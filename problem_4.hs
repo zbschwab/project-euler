8181a62ec2129b3d41f1d4918800860c2001c085
Z Schwab <zbajraktarischwab@gmail.com>
2025-07-31 01:21:40 -0400

    solved

--- problem_4.hs
+++ problem_4.hs
@@ -6,3 +6,17 @@ palindrome made from the product of two 2-digit numbers is
 Find the largest palindrome made from the product of two 
 3-digit numbers.
 -}
+
+digitList :: Int -> [Int]
+digitList n
+    | n == 0    = []
+    | otherwise = digitList (n `div` 10) ++ [n `mod` 10]
+
+isPal :: Int -> Bool
+isPal n = digitList n == reverse (digitList n)
+
+findPal :: Int
+findPal = maximum [x*y | x <- [100..999], y <- [x..999], isPal (x*y)]
+
+main :: IO()
+main = print findPal
