ff6b8b204cb5b70217bd7de33bde168ec00836c2
Z Schwab <zbajraktarischwab@gmail.com>
2025-08-09 18:41:54 -0500

    initial commit

---
+++ problem_9.hs
@@ -0,0 +1,9 @@
+-- problem 9
+{-
+A Pythagorean triplet is a set of 3 natural numbers,
+a < b < c, for which a^2 + b^2 = c^2
+
+There exists exactlys one Pythagorean triplet for which
+a + b + c = 1000. Find the product abc.
+-}
+
