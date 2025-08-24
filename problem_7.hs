665d238d74aa67f38b4ae51c5cd9c51683278fea
Z Schwab <zbajraktarischwab@gmail.com>
2025-08-24 18:42:50 -0500

    solved

--- problem_7.hs
+++ problem_7.hs
@@ -5,3 +5,10 @@ we can see that the 6th prime is 13.
 
 What is the 10,001st prime?
 -}
+
+main :: IO ()
+main = print (primes !! 10000)
+
+primes = sieve [2..]
+
+sieve (p:ps) = p : sieve [ x | x <- ps, mod x p /= 0 ]
