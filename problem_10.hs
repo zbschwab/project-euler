28f5936d9b591b1588519691ca805860676066f9
Z Schwab <zbajraktarischwab@gmail.com>
2025-08-25 13:40:20 -0500

    attempted solution, takes 30+mins to run

--- problem_10.hs
+++ problem_10.hs
@@ -4,3 +4,10 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below 2 million.
 -}
+
+main :: IO ()
+main = print (takeWhile (< 2000000) primes)
+
+primes = sieve [2 ..]
+
+sieve (p : ps) = p : sieve [x | x <- ps, mod x p /= 0]
